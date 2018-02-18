e00979ddf515d7aca466fc1d9a9e592590b2afdc
Titusz Pan <titusz.pan@gmail.com>
2018-02-18 23:07:13 +0100

    models: block detail, absolute urls, standard formatting, remove streams

--- mex/models.py
+++ mex/models.py
@@ -1,30 +1,36 @@
 # -*- coding: utf-8 -*-
-from django.contrib.postgres.fields import JSONField, ArrayField
-from django.core.serializers.json import DjangoJSONEncoder
 from django.db import models
 from django.db.models import SET_NULL, CASCADE
+from django.urls import reverse
+
 from mex import fields
+from mex.querysets import AddressQuerySet
 
 
 class Block(models.Model):
 
+    height = models.PositiveIntegerField(primary_key=True)
     hash = fields.HashField(max_length=32, unique=True)
-    miner = models.CharField(max_length=42)
-    height = models.PositiveIntegerField()
+    merkleroot = fields.HashField(max_length=32)
+    miner = models.ForeignKey('mex.Address', on_delete=SET_NULL, null=True)
     time = models.DateTimeField()
-    txcount = models.PositiveIntegerField()
+    txcount = models.PositiveSmallIntegerField()
+    size = models.PositiveIntegerField()
 
     class Meta:
         get_latest_by = 'height'
 
     def __str__(self):
-        return f'Block({self.height})'
+        return 'Block(%s)' % self.height
 
     def natural_key(self):
         return self.hash
 
+    def get_absolute_url(self):
+        return reverse('block-detail', args=[str(self.hash)])
+
     @classmethod
-    def get_height(cls):
+    def get_db_height(cls):
         try:
             return cls.objects.latest().height
         except Block.DoesNotExist:
@@ -47,16 +53,23 @@ class Transaction(models.Model):
     def natural_key(self):
         return self.hash
 
+    def get_absolute_url(self):
+        return reverse('transaction-detail', args=[str(self.hash)])
+
 
 class Address(models.Model):
 
     address = models.CharField(max_length=52, primary_key=True)
+    objects = AddressQuerySet.as_manager()
 
     def __str__(self):
         return self.address
 
     def natural_key(self):
         return self.address
+
+    def get_absolute_url(self):
+        return reverse('address-detail', args=[str(self.address)])
 
 
 class Output(models.Model):
@@ -76,10 +89,10 @@ class Output(models.Model):
     spent = models.BooleanField(default=False)
 
     def __str__(self):
-        return f'{self.transaction}:{self.out_idx}'
+        return '%s:%s' % (self.transaction, self.out_idx)
 
     def natural_key(self):
-        return f'{self.transaction.hash}:{self.out_idx}'
+        return '%s:%s' % (self.transaction.hash, self.out_idx)
 
 
 class Input(models.Model):
@@ -96,7 +109,7 @@ class Input(models.Model):
     coinbase = models.BooleanField()
 
     def __str__(self):
-        return f'{self.spends}'
+        return '%s' % self.spends
 
     def value(self):
         if self.coinbase:
@@ -105,22 +118,3 @@ class Input(models.Model):
             return self.spends.value
         else:
             return 0
-
-
-class Stream(models.Model):
-
-    name = models.CharField(max_length=32)
-    createtxid = fields.HashField(max_length=32, unique=True)
-    streamref = models.CharField(max_length=64)
-    open = models.BooleanField()
-    details = JSONField(encoder=DjangoJSONEncoder)
-    creators = ArrayField(models.CharField(max_length=52, blank=True), size=8)
-    subscribed = models.BooleanField()
-    synchronized = models.BooleanField()
-    items = models.IntegerField()
-    confirmed = models.IntegerField()
-    keys = models.IntegerField()
-    publishers = models.IntegerField()
-
-    def __str__(self):
-        return f'{self.name}'
