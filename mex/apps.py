dfb7c9394ef33cb9c84675c4c95e94074931e9c9
Titusz Pan <titusz.pan@gmail.com>
2018-02-18 22:46:19 +0100

    cleanup apps module and use settings for site title and header

--- mex/apps.py
+++ mex/apps.py
@@ -1,29 +1,15 @@
 # -*- coding: utf-8 -*-
+from django.conf import settings
 from django.apps import AppConfig
 from django.contrib.admin import AdminSite
-
-
-# class AccessUser(object):
-#     has_module_perms = has_perm = __getattr__ = lambda s, *a, **kw: True
 
 
 class MexConfig(AppConfig):
 
     name = 'mex'
-    verbose_name = "Mex Explorer"
+    verbose_name = settings.MEX_BRAND
 
     def ready(self):
-        """
-        Monkeypatch default admin with our own admin site
-        see: https://stackoverflow.com/a/30056258
-        """
-        from django.contrib import admin
-        from django.contrib.auth.models import User, Group
-        AdminSite.site_title = 'Content Blockchain Explorer'
-        AdminSite.site_header = 'Content Blockchain Explorer'
+        AdminSite.site_title = settings.MEX_BRAND
+        AdminSite.site_header = settings.MEX_BRAND
         AdminSite.index_title = ''
-
-        # admin.site.has_permission = lambda r: setattr(r, 'user', AccessUser()) or True
-        # admin.site.unregister(User)
-        # admin.site.unregister(Group)
-        # admin.site.disable_action('delete_selected')
