b6cdd9f113951acab3edd4e8a1495719ea489f45
Patricia Schinke <schinke@creations.de>
2018-03-24 13:13:14 +0100

    Add transaction detail view

---
+++ mex/utils.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+from _sha256 import sha256
+from binascii import unhexlify
+from hashlib import new
+
+import base58
+
+
+def public_key_to_address(public_key: str, pkhv, cv):
+    """Create Address from a public key.
+
+    Implementation of http://bit.ly/2hH1UUY
+
+    :param str public_key: hex encoded ECDSA public key
+    :param str pkhv: address-pubkeyhash-version of chain
+    :param str cv: address-checksum-value of chain
+    :return str: address
+    """
+
+    # Work with raw bytes
+    pubkey_raw = unhexlify(public_key)
+    pkhv_raw = unhexlify(pkhv)
+    cv_raw = unhexlify(cv)
+
+    # Hash public key
+    ripemd160 = new('ripemd160')
+    ripemd160.update(sha256(pubkey_raw).digest())
+    pubkey_raw_hashed = ripemd160.digest()
+
+    # Extend
+    steps = 20 // len(pkhv_raw)
+    idx = 0
+    privkey_ba = bytearray(pubkey_raw_hashed)
+    for pkhv_byte in pkhv_raw:
+        privkey_ba.insert(idx, pkhv_byte)
+        idx += steps + 1
+    pubkey_raw_extended = bytes(privkey_ba)
+
+    pubkey_raw_sha256d = sha256d(pubkey_raw_extended)
+
+    # XOR first 4 bytes with address-checksum-value for postfix
+    postfix = xor_bytes(pubkey_raw_sha256d[:4], cv_raw)
+
+    # Compose final address
+    address_bin = pubkey_raw_extended + postfix
+    return base58.b58encode(address_bin)
+
+
+def sha256d(data):
+    return sha256(sha256(data).digest()).digest()
+
+def xor_bytes(a, b):
+    result = bytearray()
+    for b1, b2 in zip(a, b):
+        result.append(b1 ^ b2)
+    return bytes(result)
--- mex/views.py
+++ mex/views.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
+import datetime
+
 from django.views.generic import DetailView, TemplateView
 from django_tables2 import MultiTableMixin, SingleTableView
 
 from mex.rpc import get_client
 from mex.tables import BlockTable, TransactionTable, AddressTable
 from mex.models import Block, Transaction, Address
+from mex.utils import public_key_to_address
 
 
 class StatusView(TemplateView):
@@ -76,6 +79,38 @@ class TransactionDetailView(DetailView):
     model = Transaction
     slug_field = 'hash'
     slug_url_kwarg = 'hash'
+
+    def get_context_data(self, **kwargs):
+        api = get_client()
+        ctx = super().get_context_data(**kwargs)
+        ctx['details'] = api.getrawtransaction(ctx['transaction'].hash, 4)
+        blockchain_params = api.getblockchainparams()
+        pubkeyhash_version = blockchain_params['address-pubkeyhash-version']
+        checksum_value = blockchain_params['address-checksum-value']
+        ctx['formattedBlocktime'] = datetime.datetime.fromtimestamp(ctx['details']['blocktime'])
+        ctx['formattedVin'] = []
+        ctx['formattedVout'] = []
+        for index, vin in enumerate(ctx['details']['vin']):
+            address = 'N/A'
+            if 'scriptSig' in vin:
+                public_key = vin['scriptSig']['asm'].split(' ')[1]
+                address = public_key_to_address(public_key, pubkeyhash_version, checksum_value)
+            ctx['formattedVin'].append({
+                'index': index,
+                'address': address,
+                'transaction': vin['txid'] if 'txid' in vin else 'Generation'
+            })
+        for index, vout in enumerate(ctx['details']['vout']):
+            address = 'N/A'
+            if 'scriptPubKey' in vout and 'addresses' in vout['scriptPubKey']:
+                address = ', '.join(vout['scriptPubKey']['addresses'])
+            ctx['formattedVout'].append({
+                'index': index,
+                'address': address,
+                'transaction': vin['txid'] if 'txid' in vin else 'Not yet redeemed',
+                'amount': vout['value']
+            })
+        return ctx
 
 
 class AddressDetailView(DetailView):
