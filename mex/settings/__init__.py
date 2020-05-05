b4e5a2ba44351c6dfe14aa93b493190490c76861
Titusz Pan <titusz.pan@gmail.com>
2020-05-05 13:59:37 +0200

    Add item counter to stream item list

--- mex/settings/__init__.py
+++ mex/settings/__init__.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django.contrib.postgres",
+    "django.contrib.humanize",
     "django_tables2",
     "django_filters",
     "django_extensions",
